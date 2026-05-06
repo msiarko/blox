b3ab937a1cc39b54727f99b895363913cb6c2a8a
Mikhail Siarko <mikhail.siarko@gmail.com>
2026-05-06 15:59:54 +0200

    Refactor genesis_prev_hash option to use a constant for hash length

--- build.zig
+++ build.zig
@@ -23,9 +23,10 @@ pub fn build(b: *std.Build) void {
     });
     mod.addImport("volt", volt.module("volt"));
 
+    const Hash = [Sha256.digest_length]u8;
     const options = b.addOptions();
     options.addOption(u4, "genesis_difficulty", 2);
-    options.addOption([Sha256.digest_length]u8, "genesis_prev_hash", [_]u8{0} ** Sha256.digest_length);
+    options.addOption(Hash, "genesis_prev_hash", @as(Hash, @splat(0)));
     options.addOption([]const u8, "genesis_data", "GENESIS");
     options.addOption(i64, "genesis_timestamp", 199_204);
     options.addOption(u64, "genesis_nonce", 3_349);
