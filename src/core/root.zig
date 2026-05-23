df6218f7370ede585049cd1fa752eb90661165b1
Mikhail Siarko <mikhail.siarko@gmail.com>
2026-05-23 16:06:05 +0200

    Update dependencies and enhance transaction handling in Wallet and TransactionPool

--- src/core/root.zig
+++ src/core/root.zig
@@ -1,6 +1,7 @@
 pub const Blockchain = @import("Blockchain.zig");
 pub const Wallet = @import("Wallet.zig");
 pub const Transaction = @import("Transaction.zig");
+pub const TransactionPool = @import("TransactionPool.zig");
 
 test {
     const refAllDecls = @import("std").testing.refAllDecls;
@@ -8,4 +9,5 @@ test {
     _ = refAllDecls(Blockchain.Block);
     _ = refAllDecls(Wallet);
     _ = refAllDecls(Transaction);
+    _ = refAllDecls(TransactionPool);
 }
